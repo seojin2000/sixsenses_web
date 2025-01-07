4ab21be587d6e44d248564f3c4d1e2e0036b24eb
heozzi <hz0819@naver.com>
2025-01-07 17:31:34 +0900

    아이폰크기 변경

--- src/pages/MyInput.jsx
+++ src/pages/MyInput.jsx
@@ -2,18 +2,13 @@
  * 1. 로그인(닉네임 페이지 관련 코드)
  *  - 공백,중복 체크 확인하기
  */
-import {useNavigate} from 'react-router-dom'
-import React from "react";
-import '../TailWindStyle.css';
-
-
-import {  
-    useState,       // 상태변수 -> 화면갱신
-} from "react";
+import { useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import "../TailWindStyle.css";
 
 function MyInput() {
     // 별명(닉네임) 입력 초기 설정
-    const [nickname,setnickname] = useState("");
+    const [nickname, setnickname] = useState("");
 
     // 다음 페이지 전달
     const navigate = useNavigate();
@@ -21,25 +16,24 @@ function MyInput() {
     // input 값 입력시 변경되게 설정
     function onChangeHandler(evt) {
         let ori_text = evt.target.value;
-        setnickname(ori_text)
-
+        setnickname(ori_text);
     }
     // 제출했을 때 반응하는 핸들러
     function onSubmitHandler(evt) {
         evt.preventDefault();
-        // 공백 체크 
-        if (nickname.length===0) {
-            alert("닉네임을 입력해주세요")
+        // 공백 체크
+        if (nickname.length === 0) {
+            alert("닉네임을 입력해주세요");
         }
         // 중복 체크
         //TODO 1.추후에 유효성 체크
-        else if (nickname==='김강빈') {
+        else if (nickname === "김강빈") {
             // 추후에 유효성 검사
-            alert("다른 닉네임으로 입력해주세요")
+            alert("다른 닉네임으로 입력해주세요");
         }
         // navigate를 사용하여 다음페이지 전달
         else {
-            navigate('/Qustion',{state:{nickname}});
+            navigate("/Qustion", { state: { nickname } });
             console.log("전송", nickname);
         }
         setnickname("");
@@ -47,43 +41,42 @@ function MyInput() {
 
     return (
         // TailWindStyle CSS를 사용하여 폼 구축진행
-        <div className="flex justify-center items-center min-h-screen bg-gray-100 overflow-hidden">
-        <div className="w-[390px] h-[874px] bg-white rounded-lg shadow-lg flex flex-col justify-center items-center">
-        <div className="flex flex-col h-full p-4 w-full">
-            <form onSubmit={onSubmitHandler} className="flex flex-col justify-center items-center w-full h-full">
-                <div>
-                    <h1 className="text-4xl font-bold">사자성어 퀴즈!</h1>
+        <div className="flex justify-center items-center min-h-screen bg-gray-100">
+            <div className="w-[390px] h-[844px] bg-white rounded-lg shadow-lg flex flex-col justify-center items-center">
+                <div className="flex flex-col h-full p-4 w-full">
+                    <form onSubmit={onSubmitHandler} className="flex flex-col justify-center items-center w-full h-full">
+                        <div className="mt-4">
+                            <h1 className="text-4xl font-bold">사자성어 퀴즈!</h1>
+                        </div>
+                        <div className="pt-20 pb-6 text-center">
+                            <h1 className="text-xl font-bold">이름(별명)을 입력해주세요</h1>
+                        </div>
+                        <div className="pb-20 mb-20 w-full flex justify-center">
+                            <input
+                                className="outline outline-offset-2 
+                                            outline-black-500/50 w-3/4 
+                                            px-4 py-2 rounded-md
+                                            text-lg placeholder:text-center"
+                                type="text"
+                                value={nickname}
+                                placeholder="이름(별명)을 입력해주세요"
+                                onChange={onChangeHandler}/>
+                        </div>
+                        <div className="flex justify-center items-center mt-20">
+                            <button
+                                className="outline outline-offset-2 outline-red-500 hover:bg-red-500 
+                                    text-black transition-colors px-6 py-2 rounded-md cursor-pointer text-lg font-semibold"
+                                size="md"
+                                type="button"
+                                onClick={onSubmitHandler}>
+                                게임 시작
+                            </button>
+                        </div>
+                    </form>
                 </div>
-                <div className="pt-20 pb-6 text-center">
-                    <h1 className="text-xl font-bold">이름(별명)을 입력해주세요</h1>
-                </div>
-                <div className="pb-20 mb-20 w-full flex justify-center">
-                    <input
-                        className="outline outline-offset-2 
-                        outline-black-500/50 w-3/4 
-                        px-4 py-2 rounded-md
-                        text-lg placeholder:text-center"
-                        type="text"
-                        value={nickname}
-                        placeholder="이름(별명)을 입력해주세요"
-                        onChange={onChangeHandler}
-                    />
-                </div>
-                <div className="flex justify-center items-center mt-20 
-                    outline outline-offset-2 outline-red-500 hover:bg-red-500 
-                    :text-white transition-colors px-6 py-2 rounded-md cursor-pointer">
-                    <button
-                        className="text-lg font-semibold"
-                        size="md"
-                        type="button"
-                        onClick={onSubmitHandler}>
-                        게임 시작
-                    </button>
-                </div>
-            </form>
+            </div>
         </div>
-        </div>
-        </div>
-    );}
+    );
+}
 
-export default MyInput
+export default MyInput;
